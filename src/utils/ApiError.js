ab651c3f22c42a031100007efb408749f2846b86
Vipul Kumar Chauhan <vc252@snu.edu.in>
2025-01-10 23:11:52 +0530

    add error and response utility

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statusCode = 500,
+    message = 'something went wrong',
+    errors = [],
+    stack = ''
+  ) {
+    super(message);
+    this.errors = errors;
+    this.statusCode = statusCode;
+    this.data = null;
+    this.success = false;
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this,this.constructor);
+    }
+  }
+}
+
+export default ApiError;
