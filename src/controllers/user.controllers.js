a7883451a3487bc5b98002d9521f0c91235bc04b
Vipul Kumar Chauhan <vc252@snu.edu.in>
2025-01-18 18:12:04 +0530

    add get channel info controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -300,6 +300,82 @@ const updateCoverImage = asyncHandler( async (req,res,next) => {
   )
 })
 
+const getUserChannelProfile = asyncHandler( async (req, res, next) => {
+  const {username} = req.params;
+
+  if (!username) {
+    throw new ApiError(400,'Username not provided');
+  }
+
+  username = username?.trim();
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscriberCount: {
+          $size: "$subscribers"
+        },
+        subscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"]
+            },
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        firstName: 1,
+        lastName: 1,
+        username: 1,
+        subscriberCount:1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+      }
+    }
+  ])
+
+  if (!channel?.length) {
+    throw new ApiError(404,'channel does not exists');
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,channel[0],'User channel fetched successfully')
+  )
+})
+
 export {
   registerUser,
   loginUser,
@@ -308,5 +384,6 @@ export {
   changePassword,
   updateAccountDetails,
   updateAvatar,
-  updateCoverImage
+  updateCoverImage,
+  getUserChannelProfile
 };
